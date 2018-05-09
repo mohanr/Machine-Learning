b385dc7f72ba4cfca75a84b99458b5cf6c3630b7
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2018-05-09 17:33:26 +0530

    Update gan.py
    Refactored the discriminator and generator and changed the convolution and strided deconvolution layers. But after about 500 epochs the generated images is still unformed.

--- Generative Adversarial Networks/gan.py
+++ Generative Adversarial Networks/gan.py
@@ -4,8 +4,9 @@ from PIL import Image
 import os
 import ntpath
 
+
 # The part that discriminates
-X = tf.placeholder(tf.float32, shape=[None, 299, 299, 3], name='X')
+X = tf.placeholder(tf.float32, shape=[None, 299, 299, 1], name='X')
 
 
 # The part that generates
@@ -13,38 +14,42 @@ Z = tf.placeholder(dtype=tf.float32,
                               shape=(None, 100),
                               name='Z')
 
-filters = [2, 2, 3, 32]
-weights = tf.Variable(tf.truncated_normal(filters, stddev=0.03))
-
 
 def generator(z,reuse=False):
     with tf.variable_scope('generator',reuse=reuse):
-        linear = tf.layers.dense(z, 1024 * 4 * 4)
-        conv = tf.reshape(linear, (-1, 4, 4, 1024))
-        out = tf.layers.conv2d_transpose(conv, 3,kernel_size=4,strides=[2,2], padding='SAME')
+        linear = tf.layers.dense(z, 1024 * 8 * 8)
+        conv = tf.reshape(linear, (-1, 64, 64, 32))
+        out = tf.layers.conv2d_transpose(conv, 64,kernel_size=4,strides=2, padding='SAME')
         out = tf.nn.relu(out)
-
+        out = tf.layers.conv2d_transpose(out, 1,kernel_size=4,strides=2, padding='SAME')
+        print( out.get_shape())
+        out = tf.nn.relu(out)
         tf.nn.tanh(out)
         return out
 
 
 def discriminator(x,reuse=False):
     with tf.variable_scope('discriminator',reuse=reuse):
-        out = tf.nn.conv2d(x, weights, [1, 1, 1, 1], padding='SAME')
+        out = tf.layers.conv2d(x, filters=32, kernel_size=[5, 5], padding='SAME')
         out = tf.nn.relu(out)
-        out = tf.nn.max_pool(out, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],padding='SAME')
-        out = tf.layers.dense(out, units=128, activation=tf.nn.relu)
+        out = tf.layers.max_pooling2d(out, pool_size=[2, 2],padding='SAME', strides=2)
+        out = tf.layers.conv2d(out, filters=64, kernel_size=[5, 5], padding='SAME')
+        out = tf.nn.relu(out)
+        out = tf.layers.max_pooling2d(out, pool_size=[2, 2],padding='SAME', strides=2)
+        out = tf.layers.dense(out, units=256, activation=tf.nn.relu)
         out = tf.layers.dense(out, units=1, activation=tf.nn.sigmoid)
         return out
 
 
 GeneratedImage = generator(Z)
-print (GeneratedImage)
+
 DxL = discriminator(X)
 DgL = discriminator(GeneratedImage, reuse=True)
+
 D_Disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = DxL, labels = tf.ones_like(DxL)))
 D_Disc_loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = DgL, labels = tf.ones_like(DgL)))
 D_MainLoss = D_Disc_loss + D_Disc_loss1
+
 G_Generate_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = DgL, labels = tf.ones_like(DgL)))
 
 D_loss = tf.summary.scalar("Discriminator Loss", D_MainLoss)
@@ -75,10 +80,11 @@ def preprocess():
 
         for i in range(50):
             file, document_tensor = sess.run([key, document])
-            #print(document_tensor.shape)
             head,tail = ntpath.split(file)
+            print(head)
+            print(head.decode("utf-8") + "\\" + tail.decode("utf-8").split(".")[0] + "-resized.png")
             document_tensor = Image.fromarray(document_tensor, "RGB")
-            document_tensor.save(os.path.join(head.decode("utf-8"), "/" + tail.decode("utf-8").split(".")[0] + "-resized.png"))
+            document_tensor.save(os.path.join(head.decode("utf-8"), "\\" + tail.decode("utf-8").split(".")[0] + "-resized.png"))
 
         coord.request_stop()
         coord.join(threads)
@@ -110,11 +116,11 @@ def train():
     reader = tf.WholeFileReader()
     _, input = reader.read(filenames)
     #input = tf.Print(input,[input,tf.shape(input),"Input shape"])
-    input = tf.image.decode_png(input, channels=3)
-    input.set_shape([299, 299, 3])
+    input = tf.image.decode_png(input, channels=1)
+    input.set_shape([299, 299, 1])
 
     batch = tf.train.batch([input],
-                           batch_size=2)
+                           batch_size=20)
 
 
     init = (tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -123,12 +129,13 @@ def train():
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
         train_writer = tf.summary.FileWriter('D:/Development_Avecto/TensorFlow/logs/1/train', sess.graph)
+        tf.summary.image("Image", GeneratedImage)
         merge = tf.summary.merge_all()
 
-        for it in range(50):
+        for it in range(500):
             _, X_batch =  sess.run([input,batch])
-            summary,_ = sess.run([merge,D_optimizer], feed_dict={Z : samplefromuniformdistribution(2,100), X: X_batch})
-            summary,_ = sess.run([merge,G_optimizer],feed_dict={ Z : samplefromuniformdistribution(2,100), X: X_batch})
+            summary,_ = sess.run([merge,D_optimizer], feed_dict={Z : samplefromuniformdistribution(20,100), X: X_batch})
+            summary,_ = sess.run([merge,G_optimizer],feed_dict={ Z : samplefromuniformdistribution(20,100), X: X_batch})
 
             train_writer.add_summary(summary, it)
             train_writer.flush()
@@ -140,7 +147,7 @@ def train():
 def main():
     # os.chdir('D:/Development_Avecto/TensorFlow/train/train')
     #preprocess()
-    printtest()
+    #printtest()
     train()
 
 
